f7340b4a0439462cc93fb67bd478de7d18e5d1dd
hana-vy <t.vytn28@gmail.com>
2024-11-25 14:23:17 +0700

    test

---
+++ buoi10/bai01.js
@@ -0,0 +1,34 @@
+// /bai02
+let a=22
+let b=11
+
+let tong=a+b
+let hieu=a-b
+let tich=a*b
+let thuong=a/b
+let trungBinhCong=(a+b)/2
+console.log("Tong",tong)
+console.log("hiẹu", hieu)
+console.log("tich",tich)
+console.log("thuong",thuong)
+console.log("trungbinhcong",trungBinhCong)
+
+//cau a
+let x=3
+let y=4
+let z=6
+let bieuThuc1=x*x+2*x+1
+console.log("bieu thuc 1",bieuThuc1)
+
+
+//hàm
+
+// let a=1
+// let b=1
+
+// function tinhTong2So(soThuNhat,soThuHai){
+//     let tong=soThuNhat+soThuHai
+//     console.log("tong 2 so:",tong)
+//     return tong
+// }
+// tinhTong2So(3,2)
---
+++ buoi10/script.js
@@ -0,0 +1,68 @@
+//bai 1
+//cau a
+let a=10
+console.log("cau la",a)
+
+let newObject={
+    mauSac:"Xanh",
+    khoiLuong:"100g"
+}
+console.log(newObject)
+
+let dienThoai={
+    ten:"Iphone14",
+}
+console.log(dienThoai)
+//1f
+let dienThoai1={
+    ten:"iPhone14",
+    hangSanXuat:"Apple",
+    mauSac:"Trang",
+    gia:"1000USD"
+}
+console.log("cau 1f",dienThoai1)
+
+//1g
+dienThoai={
+    ten:"iPhone14",
+    hangSanXuat:"Apple",
+    mauSac:"Trang",
+    gia:"1000USD"}
+    console.log("cau 1g",dienThoai)
+//1h
+
+
+//bai02
+// let a=22
+// let b=11
+
+// let tong=a+b
+// let hieu=a-b
+// let tich=a*b
+// let thuong=a/b
+// let trungBinhCong=(a+b)/2
+// console.log("Tong",tong)
+// console.log("hiẹu", hieu)
+// console.log("tich",tich)
+// console.log("thuong",thuong)
+// console.log("trungbinhcong",trungBinhCong)
+
+//cau a
+let x=3
+let y=4
+let z=6
+let bieuThuc1=x*x+2*x+1
+console.log("bieu thuc 1",bieuThuc1)
+
+
+//hàm
+
+// let a=1
+// let b=1
+
+// function tinhTong2So(soThuNhat,soThuHai){
+//     let tong=soThuNhat+soThuHai
+//     console.log("tong 2 so:",tong)
+//     return tong
+// }
+// tinhTong2So(3,2)
